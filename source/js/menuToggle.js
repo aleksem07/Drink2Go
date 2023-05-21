bc9ac2b658b219a027d7ce597b12ab75bdf3901f
aleksem07 <aleksem07@gmail.com>
2023-05-21 20:52:06 +0300

    feat: add menu toggler

--- source/js/menuToggle.js
+++ source/js/menuToggle.js
@@ -1,11 +1,16 @@
-(() => {
-  const menuButton = document.querySelector(".menu-nav__button");
-  const menuList = document.querySelector(".menu-nav__list");
+document.addEventListener('DOMContentLoaded', () => {
+  (() => {
+    const menuButton = document.querySelector('.menu-nav__button');
+    const menuList = document.querySelector('.menu-nav__list');
 
-  menuButton.addEventListener("click", () => {
-    let expanded = menuButton.getAttribute("aria-expanded") === "true";
-    menuButton.setAttribute("aria-expanded", !expanded);
-    menuButton.classList.toggle("menu__button--open");
-    menuList.classList.toggle("menu__list--open");
-  });
-})();
+    menuButton.classList.remove('menu__button--open');
+    menuList.classList.remove('menu__list--open');
+
+    menuButton.addEventListener('click', () => {
+      let expanded = menuButton.getAttribute('aria-expanded') === 'true';
+      menuButton.setAttribute('aria-expanded', !expanded);
+      menuButton.classList.toggle('menu__button--open');
+      menuList.classList.toggle('menu__list--open');
+    });
+  })();
+});
